1e51578d291c595ef17c4259bfceebf1c045b325
zouqinjia <zouqinjia@163.com>
2018-06-22 17:50:39 +0800

    eureka server 高可用 & 请求认证

--- chapter2-eureka-client/src/main/java/com/example/demo/Chapter2EurekaClientApplication.java
+++ chapter2-eureka-client/src/main/java/com/example/demo/Chapter2EurekaClientApplication.java
@@ -2,7 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @EnableEurekaClient 注解替代@EnableDiscoveryClient.在Spring Cloud中，
+ * 服务发现组件有多种选择，例如Zookeeper、Consul等。@EnableDiscoveryClient为各种
+ * 服务组件提供了支持，该注解是spring-commons项目中的注解，是一个高度抽象 ;
+ * @EnableEurekaClient 表明是Eureka的Client
+ */
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Chapter2EurekaClientApplication {
 
